815369989daa3dd01a78fbcc259c9ecba8bdfd75
Nicolas Mesa <nicolasmesa@Nicolass-MacBook-Air.local>
2016-06-13 08:38:18 -0500

    finished attempt

--- js/script.js
+++ js/script.js
@@ -1,20 +1,39 @@
-// clock
 
 var startClock = function() {
     var today = new Date()
     var h = today.getHours()
+    var hours = h.toString(16)
     var m = today.getMinutes()
+    var minutes = m.toString(16)
     var s = today.getSeconds()
-    m = checkTime(m)
-    s = checkTime(s)
+    var seconds = s.toString(16)
+    m = timeCheck(m)
+    s = timeCheck(s)
     document.querySelector("#clock").innerHTML = h + ":" + m + ":" + s
-    setTimeout(startClock, 300)
-
+    setTimeout(startClock, 1000)
+    var overlay2 = document.querySelector("#overlay")
+    var backgroundHex = '#' + hours * 4 + minutes * 4 + seconds * 4
+    overlay2.style.background = backgroundHex
+    var hexContainer = document.querySelector("#hexNumb")
+    hexContainer.innerHTML = backgroundHex
+    console.log(hexNumb)
 }
 
-var checkTime = function(i) {
+var timeCheck = function(i) {
     if (i < 10) {
     i = "0" + i
     }
     return i
 }
+
+var line = document.querySelector("#line")
+    width = 0
+
+var timer = setInterval(function() {
+  line.style.width = ( width += 10 ) + "px"
+  // at 350px, line shrinks back to 0
+  if ( width === 500 ) {
+    width = 0
+    // clearInterval( timer )
+  }
+}, 1000)
